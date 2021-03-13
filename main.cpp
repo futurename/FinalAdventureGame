ad051900f979ba6ff404b0876019084a55cdbb0d
Wei <=>
2021-03-12 22:57:16 -0500

    sdl2 setup

--- main.cpp
+++ main.cpp
@@ -1,64 +1,174 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.*/
+
+//Using SDL, standard IO, and strings
 #include <SDL.h>
-#include <cmath>
+#include <stdio.h>
+#include <string>
 
-class Framework{
-public:
-    // Contructor which initialize the parameters.
-    Framework(int height_, int width_): height(height_), width(width_){
-        SDL_Init(SDL_INIT_VIDEO);       // Initializing SDL as Video
-        SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);      // setting draw color
-        SDL_RenderClear(renderer);      // Clear the newly created window
-        SDL_RenderPresent(renderer);    // Reflects the changes done in the
-        //  window.
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//Loads individual image
+SDL_Surface* loadSurface( std::string path );
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//Current displayed image
+SDL_Surface* gStretchedSurface = NULL;
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
     }
 
-    // Destructor
-    ~Framework(){
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load stretching surface
+    gStretchedSurface = loadSurface( "05_optimized_surface_loading_and_soft_stretching/stretch.bmp" );
+    if( gStretchedSurface == NULL )
+    {
+        printf( "Failed to load stretching image!\n" );
+        success = false;
     }
 
-    void draw_circle(int center_x, int center_y, int radius_){
-        // Setting the color to be RED with 100% opaque (0% trasparent).
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    return success;
+}
 
-        // Drawing circle
-        for(int x=center_x-radius_; x<=center_x+radius_; x++){
-            for(int y=center_y-radius_; y<=center_y+radius_; y++){
-                if((std::pow(center_y-y,2)+std::pow(center_x-x,2)) <=
-                   std::pow(radius_,2)){
-                    SDL_RenderDrawPoint(renderer, x, y);
-                }
-            }
+void close()
+{
+    //Free loaded image
+    SDL_FreeSurface( gStretchedSurface );
+    gStretchedSurface = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+
+SDL_Surface* loadSurface( std::string path )
+{
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+    }
+    else
+    {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
+        if( optimizedSurface == NULL )
+        {
+            printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
         }
 
-        // Show the change on the screen
-        SDL_RenderPresent(renderer);
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
     }
 
-private:
-    int height;     // Height of the window
-    int width;      // Width of the window
-    SDL_Renderer *renderer = NULL;      // Pointer for the renderer
-    SDL_Window *window = NULL;      // Pointer for the window
-};
+    return optimizedSurface;
+}
 
-int main(int argc, char * argv[]){
-
-    // Creating the object by passing Height and Width value.
-    Framework fw(200, 400);
-
-    // Calling the function that draws circle.
-    fw.draw_circle(200, 100, 50);
-
-    SDL_Event event;    // Event variable
-
-    // Below while loop checks if the window has terminated using close in the
-    //  corner.
-    while(!(event.type == SDL_QUIT)){
-        SDL_Delay(10);  // setting some Delay
-        SDL_PollEvent(&event);  // Catching the poll event.
+int main( int argc, char* args[] )
+{
+    //Start up SDL and create window
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
     }
+    else
+    {
+        //Load media
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {
+            //Main loop flag
+            bool quit = false;
+
+            //Event handler
+            SDL_Event e;
+
+            //While application is running
+            while( !quit )
+            {
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                }
+
+                //Apply the image stretched
+                SDL_Rect stretchRect;
+                stretchRect.x = 0;
+                stretchRect.y = 0;
+                stretchRect.w = SCREEN_WIDTH;
+                stretchRect.h = SCREEN_HEIGHT;
+                SDL_BlitScaled( gStretchedSurface, NULL, gScreenSurface, &stretchRect );
+
+                //Update the surface
+                SDL_UpdateWindowSurface( gWindow );
+            }
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
 }
