167eba63d5b491de8a9c23622212bf2a61b93eab
Kishan Patel <kishanpat07@gmail.com>
2021-03-15 13:45:22 -0400

    dice test #3

--- Dice.cpp
+++ Dice.cpp
@@ -1,4 +1,9 @@
 //
 // Created by Kishan on 2021-03-15.
 //
+#include "Dice.h"
 
+int RollDie(){
+
+    srand((unsigned) time(0));
+}
--- Dice.h
+++ Dice.h
@@ -2,7 +2,29 @@
 // Created by Kishan on 2021-03-15.
 //
 
+#include "imports.h"
+
 #ifndef FINALGAMEDESIGN_DICE_H
 #define FINALGAMEDESIGN_DICE_H
+
+// if player using 1 army, roll 1 die
+// if player using 2 army, roll 2 die
+// if player using > 3 army, roll
+class Dice{
+public:
+    int RollDie();
+
+    int GetMinValue();
+    int GetMaxValue();
+
+
+private:
+    int roll;
+    int min = 1;
+    int max = 6;
+
+
+};
+
 
 #endif //FINALGAMEDESIGN_DICE_H
