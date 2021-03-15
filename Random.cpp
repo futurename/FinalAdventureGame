9bcd5d7d76035a6afbfa61bb8d1ddd9787f96c43
Kishan Patel <kishanpat07@gmail.com>
2021-03-15 16:16:59 -0400

    added random number functionality

--- Random.cpp
+++ Random.cpp
@@ -1,13 +1,20 @@
 //
 // Created by Kishan on 2021-03-15.
 //
-#include "Dice.h"
+#include "Random.h"
 #include <cstdlib>
 #include <ctime>
 
-int Dice::RollDie(){
+int Random::RollDie(){
     srand((unsigned) time(0));
     int roll;
     roll = (rand() % maxValue) + minValue;
     return roll;
+}
+
+int Random::GenerateRandomNum(){
+    srand((unsigned) time(nullptr));
+    int rand;
+    rand = (rand() % 42) + 1;
+    return rand;
 }
--- Random.h
+++ Random.h
@@ -2,19 +2,21 @@
 // Created by Kishan on 2021-03-15.
 //
 
-#ifndef FINALGAMEDESIGN_DICE_H
-#define FINALGAMEDESIGN_DICE_H
+#ifndef FINALGAMEDESIGN_RANDOM_H
+#define FINALGAMEDESIGN_RANDOM_H
 
 // if player using 1 army, roll 1 die
 // if player using 2 army, roll 2 die
 // if player using > 3 army, roll
-class Dice{
+
+class Random{
 public:
     int RollDie();
+    int GenerateRandomNum(){
     int roll;
     int maxValue = 6;
     int minValue = 1;
 
 };
 
-#endif //FINALGAMEDESIGN_DICE_H
+#endif //FINALGAMEDESIGN_RANDOM_H
