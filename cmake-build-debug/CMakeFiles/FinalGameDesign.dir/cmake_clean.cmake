6ce129548d9e6f3b279e1d92f7922e33923a2002
passe-partout <ji.shen.ca@gmail.com>
2021-03-17 16:42:37 -0400

    created Text Color Branch

---
+++ cmake-build-debug/CMakeFiles/FinalGameDesign.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalGameDesign.dir/Card.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/Continent.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/Country.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/HelperFunctions/ColorList.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/HelperFunctions/Game.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/HelperFunctions/MapManager.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/Player.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/Random.cpp.obj"
+  "CMakeFiles/FinalGameDesign.dir/main.cpp.obj"
+  "FinalGameDesign.exe"
+  "FinalGameDesign.exe.manifest"
+  "FinalGameDesign.pdb"
+  "libFinalGameDesign.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalGameDesign.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
